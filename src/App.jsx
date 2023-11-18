fd699073b21ab02b97437b6e2fbfc5fc49b55db9
GCortesGustavo <gustavocortes934@gmail.com>
2023-11-17 23:02:20 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-xl bg-black text-cyan-400'>Welcome to my portfolio Probando un cambio</h1>
+    </>
+  )
+}
+
+export default App
