77621068dfd43e7dbdba206b5b61b606c3109b63
GCortesGustavo <gustavocortes934@gmail.com>
2023-11-30 18:12:55 -0300

    Cambiando flujo de informacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ function App() {
 
   return (
     <>
-      <div className='bg-grey br-10'>
-        <h1>Menu</h1>
+      <div className='bg-green'>
+        <h1>Menu improvisado
+        </h1>
         <h1>About</h1>
         <h1>Projects</h1>
         <h1>Contact</h1>
         <p>Oscuro Claro</p>
         <p>Idioma</p>
       </div>
+
       <div>ABOUT</div>
       <div>PROJECTS</div>
       <div>CONTACT</div>
