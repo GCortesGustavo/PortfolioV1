49c67bc612be668f9964ccf8dc35e695017cc362
GCortesGustavo <gustavocortes934@gmail.com>
2023-12-05 19:49:37 -0300

    Navbar casi terminada y preparada para merge

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,19 +2,19 @@
 
 const Navbar = () => {
     return (
-        <div className="flex bg-primary flex-row justify-around">
+        <div className="flex  flex-row justify-around">
             <div className="flex flex-row space-beetween">
-                <p>Imagen</p>
+                <p className="m-7">Imagen</p>
             </div>
-            <div className="flex flex-row space-beetween">
-                <button className="m-7 hover:bg-black">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <div className="flex flex-row space-beetween ">
+                <button className="m-7 hover:bg-black bg-primary rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
                         <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                    </svg>  
+</svg> 
                 </button>
-                <button className="m-7 hover:bg-black">
+                <button className="m-7 hover:bg-black bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-devices" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M13 9a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1v-10z" />
@@ -22,7 +22,7 @@ const Navbar = () => {
                         <path d="M16 9h2" />
                     </svg>
                 </button>
-                <button className="m-7 hover:bg-black">
+                <button className="m-7 hover:bg-black bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-tools" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" />
@@ -33,14 +33,14 @@ const Navbar = () => {
                         <path d="M16 17l-1.5 1.5" />
                     </svg>
                 </button>
-                <button className="m-7 hover:bg-black">
+                <button className="m-7 hover:bg-black bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
                         <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
                     </svg>
                 </button>
-                <button className="m-7 hover:bg-black">
+                <button className="m-7 hover:bg-black bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone-call" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
@@ -50,7 +50,7 @@ const Navbar = () => {
                 </button>
             </div>
             <div className="flex flex-row space-x-18">
-                <button className="m-7">
+                <button className="m-7 bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
@@ -60,7 +60,7 @@ const Navbar = () => {
                         <path d="M12.5 3a17 17 0 0 1 0 18" />
                     </svg>
                 </button>
-                <button className="m-7">
+                <button className="m-7 bg-primary rounded-xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shadow" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#57C5B6" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
