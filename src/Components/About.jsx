b4443c6409cc411856d91f8092357bdd30040693
Gustavo Cortes <gustavocortes934@gmail.com>
2023-12-06 19:17:34 -0300

    Armando estructura de textos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,12 +7,16 @@ const About = () => {
                 <img src="" alt="Gustavo" />
             </div>
             <div>
+                <div>
                 <h1>Soy Gustavo Cortés</h1>
+                </div>
+                <div>
                 <p>Hola a todos!
                 Soy desarrollador web, técnico en Marketing y diseñador UX/UI. 
                 Dedicado, responsable, creativo, son algunas de mis cualidades como profesional.
                 Me encanta estar al aire libre, conocer personas, crecer constantemente y aprender.
                 </p>
+                </div>
             </div>
 
         </div>
